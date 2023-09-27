d090e9b0b90b1b50c0a435542c6ffa4882f9338f
alann13 <53200557+alann13@users.noreply.github.com>
2023-09-27 12:46:55 -0400

    landing page work

---
+++ src/content/mainPage.ts
@@ -0,0 +1,27 @@
+import passionateImage from '@images/home/desktop/illustration-passionate.svg'
+import friendlyImage from '@images/home/desktop/illustration-friendly.svg'
+import resourcefulImage from '@images/home/desktop/illustration-resourceful.svg'
+
+export const contentQualities = [
+  {
+    cardHeading: 'passionate',
+    cardText:
+      'Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions.',
+    imgAltText: 'passionate icon',
+    imgSrc: passionateImage.src,
+  },
+  {
+    cardHeading: 'resourceful',
+    cardText:
+      'Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients’ needs.',
+    imgAltText: 'resourceful icon',
+    imgSrc: resourcefulImage.src,
+  },
+  {
+    cardHeading: 'friendly',
+    cardText:
+      ' We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide.',
+    imgAltText: 'friendly icon',
+    imgSrc: friendlyImage.src,
+  },
+]
