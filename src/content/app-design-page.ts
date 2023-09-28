b75d3dfe16f27d2f36f64f83cf0bd0829e373bd0
alann13 <53200557+alann13@users.noreply.github.com>
2023-09-28 09:10:08 -0400

    page work

---
+++ src/content/app-design-page.ts
@@ -0,0 +1,37 @@
+import AirfilterImage from '@images/app-design/desktop/image-airfilter.jpg'
+
+export const appDesignProjects = [
+  {
+    imageSrc: AirfilterImage.src,
+    imageAlt: 'Airfilter project',
+    captionHeading: 'airfilter',
+    captionText:
+      'Solving the problem of poor indoor air quality by filtering the air',
+  },
+  {
+    imageSrc: AirfilterImage.src,
+    imageAlt: 'eyecam project',
+    captionHeading: 'eyecam',
+    captionText:
+      'Product that lets you edit your favorite photos and videos at any time',
+  },
+  {
+    imageSrc: AirfilterImage.src,
+    imageAlt: 'faceit project',
+    captionHeading: 'faceit',
+    captionText:
+      'Get to meet your favorite internet superstar with the faceit app',
+  },
+  {
+    imageSrc: AirfilterImage.src,
+    imageAlt: 'todo project',
+    captionHeading: 'todo',
+    captionText: 'A todo app that features cloud sync with light and dark mode',
+  },
+  {
+    imageSrc: AirfilterImage.src,
+    imageAlt: 'loopstudios project',
+    captionHeading: 'loopstudios',
+    captionText: 'A VR experience app made for Loopstudios',
+  },
+]
